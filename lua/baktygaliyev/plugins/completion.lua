e40785c7f8b11e743be41b8225a315f35677480f
Bakdaulet Baktygaliyev <bakdauletbaktygaliyev@gmail.com>
2024-08-05 15:45:01 +0500

    commit

--- lua/baktygaliyev/plugins/completion.lua
+++ lua/baktygaliyev/plugins/completion.lua
@@ -2,15 +2,13 @@ return {
     {
         "hrsh7th/cmp-nvim-lsp"
     },
-
     {
         "L3MON4D3/LuaSnip",
         dependencies = {
-            "cmp_luasnip",
+            "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets"
         }
     },
-
     {
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
